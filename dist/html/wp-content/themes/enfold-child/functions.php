e613f4dc6c1a1ea66822a0bb1e2dafae8f363809
Joshua Coleman <jlcolema@me.com>
2017-04-09 21:06:07 -0500

    Add new markup for global search within header

--- dist/html/wp-content/themes/enfold-child/functions.php
+++ dist/html/wp-content/themes/enfold-child/functions.php
@@ -47,98 +47,234 @@ add_filter('upload_mimes', 'my_myme_types', 1, 1);
 
 		$styles["WormGUIDES"] = array(
 
-			/* Default
+			/* Color Scheme
+			--------------------------------------*/
+
+			// Name of scheme within list of choices
+
+			'color_scheme'							=> 'WormGUIDES',
+
+			/* Title
 			--------------------------------------*/
 
 			'style'									=> 'color: #ffffff; background-color: #2e9941;',
-			'default_font'							=> 'Open Sans:400,600',
-			'google_webfont'						=> 'Playfair Display:400,700',
-			'color_scheme'							=> 'WormGUIDES',
 
-			/* Header
+			/* Fonts
 			--------------------------------------*/
 
+			'default_font'							=> 'Open Sans:400,600',
+			'google_webfont'						=> 'Playfair Display:400,700',
+
+			/* Header (Logo Area)
+			--------------------------------------*/
+
+			// Background color
 			'colorset-header_color-bg'              => '#ffffff',
-			'colorset-header_color-bg2'             => '#f8f8f8',
-			'colorset-header_color-primary'         => '#9cc2df',
-			'colorset-header_color-secondary'       => '#9cc3df',
-			'colorset-header_color-color'           => '#000000',
-			'colorset-header_color-border'          => '#ebebeb',
+
+			// Alternate background color
+			'colorset-header_color-bg2'             => '#eeeeee',
+
+			// Primary color
+			'colorset-header_color-primary'         => '#2e9941',
+
+			// Hightlight color
+			'colorset-header_color-secondary'       => '#7bcd89',
+
+			// Font color
+			'colorset-header_color-color'           => '#333333',
+
+			// Secondary font color
+			'colorset-header_color-meta'            => '#cccccc',
+
+			// Heading color
+			'colorset-header_color-heading'         => '#333333',
+
+			// Border color
+			'colorset-header_color-border'          => '#cccccc',
+
+			// Background image
 			'colorset-header_color-img'             => '',
+
+			// Custom background image
 			'colorset-header_color-customimage'     => '',
+
+			// Position of the image
 			'colorset-header_color-pos'             => 'center center',
+
+			// Repeat
 			'colorset-header_color-repeat'          => 'repeat',
+
+			// Attachment
 			'colorset-header_color-attach'          => 'scroll',
-			'colorset-header_color-heading'         => '#000000',
-			'colorset-header_color-meta'            => '#969696',
 
 			/* Main
 			--------------------------------------*/
 
+			// Background color
 			'colorset-main_color-bg'                => '#ffffff',
+
+			// Alternate background color
 			'colorset-main_color-bg2'               => '#f8f8f8',
+
+			// Primary color
 			'colorset-main_color-primary'           => '#000000',
+
+			// Highlight color
 			'colorset-main_color-secondary'         => '#9cc3df',
+
+			// Font color
 			'colorset-main_color-color'             => '#b0b0b0',
-			'colorset-main_color-border'            => '#ebebeb',
-			'colorset-main_color-img'               => '',
-			'colorset-main_color-customimage'       => '',
-			'colorset-main_color-pos'               => 'center center',
-			'colorset-main_color-repeat'            => 'repeat',
-			'colorset-main_color-attach'            => 'scroll',
-			'colorset-main_color-heading'           => '#222222',
+
+			// Secondary font color
 			'colorset-main_color-meta'              => '#919191',
+
+			// Heading color
+			'colorset-main_color-heading'           => '#222222',
+
+			// Border color
+			'colorset-main_color-border'            => '#ebebeb',
+
+			// Background image
+			'colorset-main_color-img'               => '',
+
+			// Custom background image
+			'colorset-main_color-customimage'       => '',
+
+			// Position of the image
+			'colorset-main_color-pos'               => 'center center',
+
+			// Repeat
+			'colorset-main_color-repeat'            => 'repeat',
+
+			// Attachment
+			'colorset-main_color-attach'            => 'scroll',
+
 
 			/* Alternate
 			--------------------------------------*/
 
+			// Background color
 			'colorset-alternate_color-bg'           => '#f8f8f8',
+
+			// Alternate background color
 			'colorset-alternate_color-bg2'          => '#ffffff',
+
+			// Primary color
 			'colorset-alternate_color-primary'      => '#000000',
+
+			// Highlight color
 			'colorset-alternate_color-secondary'    => '#9cc3df',
+
+			// Font color
 			'colorset-alternate_color-color'        => '#999999',
-			'colorset-alternate_color-border'       => '#ebebeb',
-			'colorset-alternate_color-img'          => '',
-			'colorset-alternate_color-customimage'  => '',
-			'colorset-alternate_color-pos'          => 'center center',
-			'colorset-alternate_color-repeat'       => 'repeat',
-			'colorset-alternate_color-attach'       => 'scroll',
-			'colorset-alternate_color-heading'      => '#222222',
+
+			// Secondary font color
 			'colorset-alternate_color-meta'         => '#919191',
+
+			// Heading color
+			'colorset-alternate_color-heading'      => '#222222',
+
+			// Border color
+			'colorset-alternate_color-border'       => '#ebebeb',
+
+			// Background image
+			'colorset-alternate_color-img'          => '',
+
+			// Custom background image
+			'colorset-alternate_color-customimage'  => '',
+
+			// Position of the image
+			'colorset-alternate_color-pos'          => 'center center',
+
+			// Repeat
+			'colorset-alternate_color-repeat'       => 'repeat',
+
+			// Attachment
+			'colorset-alternate_color-attach'       => 'scroll',
 
 			/* Footer
 			--------------------------------------*/
 
-			'colorset-footer_color-bg'              => '#35383c',
+			// Background color
+			'colorset-footer_color-bg'              => '#2e9941', // $primary-color
+
+			// Alternate background color
 			'colorset-footer_color-bg2'             => '#52565c',
+
+			// Primary color
 			'colorset-footer_color-primary'         => '#ffffff',
-			'colorset-footer_color-secondary'       => '#ebebeb',
+
+			// Highlight color
+			'colorset-footer_color-secondary'       => '#4cb15e',
+
+			// Font color
 			'colorset-footer_color-color'           => '#e8e8e8',
-			'colorset-footer_color-border'          => '#35383c',
-			'colorset-footer_color-img'             => '',
-			'colorset-footer_color-customimage'     => '',
-			'colorset-footer_color-pos'             => 'center center',
-			'colorset-footer_color-repeat'          => 'repeat',
-			'colorset-footer_color-attach'          => 'scroll',
+
+			// Secondary font color
+			'colorset-footer_color-meta'            => '#ff0000',
+
+			// Heading color
 			'colorset-footer_color-heading'         => '#ffffff',
-			'colorset-footer_color-meta'            => '#777777',
+
+			// Border colors
+			'colorset-footer_color-border'          => '#2e9941', // $primary-color
+
+			// Background image
+			'colorset-footer_color-img'             => '',
+
+			// Custom background image
+			'colorset-footer_color-customimage'     => '',
+
+			// Position of the image
+			'colorset-footer_color-pos'             => 'center center',
+
+			// Repeat
+			'colorset-footer_color-repeat'          => 'repeat',
+
+			// Attachment
+			'colorset-footer_color-attach'          => 'scroll',
 
 			/* Socket
 			--------------------------------------*/
 
-			'colorset-socket_color-bg'              => '#282b2e',
+			// Background color
+			'colorset-socket_color-bg'              => '#18802a', // $primary-color-dark
+
+			// Alternate background color
 			'colorset-socket_color-bg2'             => '#52565c',
+
+			// Primary color
 			'colorset-socket_color-primary'         => '#ffffff',
+
+			// Highlight color
 			'colorset-socket_color-secondary'       => '#ebebeb',
+
+			// Font color
 			'colorset-socket_color-color'           => '#e8e8e8',
-			'colorset-socket_color-border'          => '#282b2e',
-			'colorset-socket_color-img'             => '',
-			'colorset-socket_color-customimage'     => '',
-			'colorset-socket_color-pos'             => 'center center',
-			'colorset-socket_color-repeat'          => 'repeat',
-			'colorset-socket_color-attach'          => 'scroll',
-			'colorset-socket_color-heading'         => '#ffffff',
+
+			// Secondary font color
 			'colorset-socket_color-meta'            => '#777777',
+
+			// Heading color
+			'colorset-socket_color-heading'         => '#ffffff',
+
+			// Border color
+			'colorset-socket_color-border'          => '#18802a', // $primary-color-dark
+
+			// Background image
+			'colorset-socket_color-img'             => '',
+
+			// Custom background image
+			'colorset-socket_color-customimage'     => '',
+
+			// Position of the image
+			'colorset-socket_color-pos'             => 'center center',
+
+			// Repeat
+			'colorset-socket_color-repeat'          => 'repeat',
+
+			// Attach
+			'colorset-socket_color-attach'          => 'scroll',
 
 			/* Body Background
 			--------------------------------------*/
